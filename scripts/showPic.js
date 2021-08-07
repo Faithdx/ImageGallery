d6d0ce0aa16aba7561d37bd7341ed2ea90e1b3c9
hhh <2375740452@qq.com>
2021-08-07 23:29:56 +0800

    first commit

---
+++ scripts/showPic.js
@@ -0,0 +1,49 @@
+function showPic(whichpic) {
+	if (!document.getElementById('placeholder')) return false
+	const source = whichpic.getAttribute('href')
+	const placeholder = document.getElementById('placeholder')
+	if (placeholder.nodeName != 'IMG') return false
+	placeholder.setAttribute('src', source)
+
+	if (document.getElementById('description')) {
+		const text = whichpic.getAttribute('title') ? whichpic.getAttribute('title') : ''
+		const description = document.getElementById('description')
+		if (description.firstChild.nodeType == 3) {
+			description.firstChild.nodeValue = text
+		}
+	}
+	return true
+}
+
+function prepareGallery() {
+	if (!document.getElementsByTagName) {
+		return false
+	}
+	if (!document.getElementById) {
+		return false
+	}
+	if (!document.getElementById('imagegallery')) {
+		return false
+	}
+	const gallery = document.getElementById('imagegallery')
+	const links = gallery.getElementsByTagName('a')
+	for (let i = 0; i < links.length; i++) {
+		links[i].onclick = function () {
+			return !showPic(this)
+		}
+	}
+}
+
+function addLoadEvent(func) {
+	let oldonload = window.onload
+	if (typeof window.onload != 'function') {
+		window.onload = func
+	} else {
+		window.onload = function () {
+			oldonload()
+			func()
+		}
+	}
+}
+
+addLoadEvent(prepareGallery)
